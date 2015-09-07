8df2d03ad4840211285bc0014fadbb882d57c434
Prashant Shrestha <pms@engineer.com>
2015-09-07 16:30:18 -0400

    Initialized a new Project, renamed files & installed Metro Framework.

---
+++ Hasifer/Hasifer/Main.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Hasifer
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroLink1 = new MetroFramework.Controls.MetroLink();
+            this.SuspendLayout();
+            // 
+            // metroLink1
+            // 
+            this.metroLink1.Location = new System.Drawing.Point(316, 219);
+            this.metroLink1.Name = "metroLink1";
+            this.metroLink1.Size = new System.Drawing.Size(197, 23);
+            this.metroLink1.TabIndex = 0;
+            this.metroLink1.Text = "Copyright © 2015, Scarecrow [CK]";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 265);
+            this.Controls.Add(this.metroLink1);
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Resizable = false;
+            this.ShadowType = MetroFramework.Forms.MetroForm.MetroFormShadowType.DropShadow;
+            this.Text = "Hasifer - Hash Identifier";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroLink metroLink1;
+    }
+}
+
